542f83031cc2cf5ac55d4fb4754a6ccd690739b4
kawsarm104 <kawsarm104@gmail.com>
2021-08-14 02:49:38 +0600

    changes backpack section color

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log("working")
 
 var h2tag = document.getElementsByTagName("h2");
-for (let i = 0; i < h2tag.length; i++) {
-    h2tag[i].style.color = "lightblue";
+// for (let i = 0; i < h2tag.length; i++) {
+//     h2tag[i].style.color = "lightblue";
+// }
+for (let h2 of h2tag) {
+    h2.style.color = "lightblue";
 }
+document.getElementById('backpack').style.backgroundColor = "tomato"
