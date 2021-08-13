7d65ab2e582f753d8aa281837027230324bbb048
kawsarm104 <kawsarm104@gmail.com>
2021-08-14 02:53:59 +0600

    changes card border to 30px

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log("working")
-
+// task 1
 var h2tag = document.getElementsByTagName("h2");
 // for (let i = 0; i < h2tag.length; i++) {
 //     h2tag[i].style.color = "lightblue";
@@ -7,4 +7,10 @@ var h2tag = document.getElementsByTagName("h2");
 for (let h2 of h2tag) {
     h2.style.color = "lightblue";
 }
+// task 2
 document.getElementById('backpack').style.backgroundColor = "tomato"
+// task 3
+const card = document.getElementsByClassName('card')
+for(let c of card){
+    c.style.borderRadius = "30px"
+}
