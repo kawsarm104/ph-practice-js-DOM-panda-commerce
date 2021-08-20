174a0406e296b6df168e998df369c169e66d8739
kawsarm104 <kawsarm104@gmail.com>
2021-08-20 13:40:54 +0600

    added new function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("working")
 // task 1
-var h2tag = document.getElementsByTagName("h2");
+const h2tag = document.getElementsByTagName("h2");
 // for (let i = 0; i < h2tag.length; i++) {
 //     h2tag[i].style.color = "lightblue";
 // }
@@ -14,3 +14,33 @@ const card = document.getElementsByClassName('card')
 for(let c of card){
     c.style.borderRadius = "30px"
 }
+// task 4
+function consolelog(){
+    console.log("Submit button clicked")
+}
+// task 5 
+// const html = document.getElementsByTagName("html")
+
+// addEventListener("click", function(){
+//     for (let btn of html) {
+//     btn.style.display = "none";
+//     }
+// });
+// document.getElementsByTagName('button').addEventListener('click', function (event) {
+//     event.target.parentNode.removeChild(event.target);
+// })
+// task 6 
+document.getElementById('input-field').
+addEventListener('keyup',function(event){
+    // const input = document.getElementById('input')
+    // console.log(input.value)
+    console.log(event.target.value)
+    if(event.target.value == 'email'){
+        document.getElementById('submit-button').style.color= "green"
+         document.getElementById('submit-button').removeAttribute('disabled')
+    }
+    else{
+        document.getElementById('submit-button').style.color= ""
+        document.getElementById('submit-button').setAttribute('disabled',true)
+    }
+})
